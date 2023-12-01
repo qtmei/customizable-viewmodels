42a8b6b471b5a8d70b4129873d75d0644ccaddc8
Mei <92554133+qtmei@users.noreply.github.com>
2023-11-30 18:25:11 -0600

    Update cs_custom_viewmodels.lua

--- lua/autorun/client/cs_custom_viewmodels.lua
+++ lua/autorun/client/cs_custom_viewmodels.lua
@@ -3,7 +3,7 @@ CreateClientConVar("viewmodel_offset_x", 0, true, false, "")
 CreateClientConVar("viewmodel_offset_y", 0, true, false, "")
 CreateClientConVar("viewmodel_offset_z", 0, true, false, "")
 
-hook.Add("Think", "CSGO_Custom_ViewModels_Think", function()
+hook.Add("Think", "CS_Custom_ViewModels_Think", function()
 	local localplayer = LocalPlayer()
 	local wep = localplayer:GetActiveWeapon()
 
@@ -20,7 +20,7 @@ hook.Add("Think", "CSGO_Custom_ViewModels_Think", function()
 	end
 end)
 
-hook.Add("CalcViewModelView", "CSGO_Custom_ViewModels_CalcViewModelView", function(wep, vm, oldPos, oldAng, pos, ang)
+hook.Add("CalcViewModelView", "CS_Custom_ViewModels_CalcViewModelView", function(wep, vm, oldPos, oldAng, pos, ang)
 	local offset = Vector(GetConVar("viewmodel_offset_x"):GetFloat(), GetConVar("viewmodel_offset_y"):GetFloat(), GetConVar("viewmodel_offset_z"):GetFloat())
 
 	offset:Rotate(ang)
