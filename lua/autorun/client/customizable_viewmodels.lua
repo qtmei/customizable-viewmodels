b597652dc50df469a03ad5b06580d833acc2b6f0
Mei <92554133+qtmei@users.noreply.github.com>
2023-12-13 12:38:55 -0600

    Update customizable_viewmodels.lua

--- lua/autorun/client/customizable_viewmodels.lua
+++ lua/autorun/client/customizable_viewmodels.lua
@@ -3,11 +3,7 @@ CreateClientConVar("viewmodel_offset_x", 0)
 CreateClientConVar("viewmodel_offset_y", 0)
 CreateClientConVar("viewmodel_offset_z", 0)
 
-hook.Add("Think", "Customizable_ViewModels_Think", function()
-	local wep = LocalPlayer():GetActiveWeapon()
-
-	if !IsValid(wep) then return end
-
+hook.Add("CalcViewModelView", "Customizable_ViewModels_CalcViewModelView", function(wep, vm, oldPos, oldAng, pos, ang)
 	if !isbool(wep.OldViewModelFlip) then
 		wep.OldViewModelFlip = wep.ViewModelFlip or false
 	end
@@ -17,9 +13,7 @@ hook.Add("Think", "Customizable_ViewModels_Think", function()
 	else
 		wep.ViewModelFlip = !wep.OldViewModelFlip
 	end
-end)
 
-hook.Add("CalcViewModelView", "Customizable_ViewModels_CalcViewModelView", function(wep, vm, oldPos, oldAng, pos, ang)
 	local offset = Vector(GetConVar("viewmodel_offset_x"):GetFloat(), GetConVar("viewmodel_offset_y"):GetFloat(), GetConVar("viewmodel_offset_z"):GetFloat())
 
 	offset:Rotate(ang)
