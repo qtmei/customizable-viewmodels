fd1da4d88cf837df2a263313ad7eab88807fe0b6
Mei <92554133+qtmei@users.noreply.github.com>
2023-12-15 23:45:26 -0600

    Update customizable_viewmodels.lua

--- lua/autorun/client/customizable_viewmodels.lua
+++ lua/autorun/client/customizable_viewmodels.lua
@@ -5,7 +5,7 @@ CreateClientConVar("viewmodel_offset_z", 0)
 
 hook.Add("CalcViewModelView", "Customizable_ViewModels_CalcViewModelView", function(wep, vm, oldPos, oldAng, pos, ang)
 	if !isbool(wep.OldViewModelFlip) then
-		wep.OldViewModelFlip = wep.ViewModelFlip or false
+		wep.OldViewModelFlip = wep.ViewModelFlip
 	end
 
 	if tobool(GetConVar("cl_righthand"):GetInt()) then
